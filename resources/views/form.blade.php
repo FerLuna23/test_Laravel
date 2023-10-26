b0b847a91282f5427fe30565e4f60aa39d6b384a
unknown <hernandezabreu96@gmail.com>
2023-10-26 14:41:46 -0600

    firts push

---
+++ resources/views/form.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>NEWSROOM - Free Bootstrap Magazine Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">   
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Topbar Start -->
+    <div class="container-fluid">
+        <div class="row align-items-center py-2 px-lg-5">
+            <div class="col-lg-4">
+                <a href="" class="navbar-brand d-none d-lg-block">
+                    <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+    <!-- Navbar Start -->
+    <div class="container-fluid p-0 mb-3">
+        <nav class="navbar navbar-expand-lg bg-light navbar-light py-2 py-lg-0 px-lg-5">
+            <a href="" class="navbar-brand d-block d-lg-none">
+                <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between px-0 px-lg-3" id="navbarCollapse">
+                <div class="navbar-nav mr-auto py-0">
+                    <a href="{{ url('/') }}" class="nav-item nav-link active">Regresar</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- Navbar End -->
+
+    <div class="container-fluid py-3">
+        <div class="container">
+            <div class="bg-light py-2 px-4 mb-3">
+                <h3 class="m-0">Registrar nueva entrada</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="contact-form bg-light mb-3" style="padding: 30px;">
+                        <div id="success"></div>
+                        <form action="{{ url('/entries') }}" name="sentMessage" id="contactForm" novalidate="novalidate" method="post">
+                            <div class="form-row">
+                                <div class="col-md-12">
+                                    <label for="title"><strong>Titulo</strong><b style="color: red;">*</b></label>
+                                    <input type="text" class="form-control p-4" id="title" name="title" required="required"/>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="author"><strong>Autor</strong><b style="color: red;">*</b></label>
+                                    <input type="text" class="form-control p-4" id="author" name="author" required="required"/>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="publication_date"><strong>Fecha de publicacion</strong><b style="color: red;">*</b></label>
+                                    <input type="date" class="form-control p-4" id="publication_date" name="publication_date" required="required"/>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="col-md-12">
+                                    <label for="content"><strong>Contenido</strong><b style="color: red;">*</b></label>
+                                    <textarea class="form-control" rows="8" id="content" name="content" required="required"></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary font-weight-semi-bold px-4" style="height: 50px;" type="submit" id="btn_guardar">Guardar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="{{ asset('mail/jqBootstrapValidation.min.js') }}"></script>
+    <script src="{{ asset('mail/contact.js') }}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('inicio');
+});
+
+Route::get('/create-entries', function () {
+    return view('form');
+});
+
+Route::post('/entries', function (Request $request) {
+    $response = Http::post('http://tu-servicio-rest/api/entries', [
+        'title' => $request->input('title'),
+        'author' => $request->input('author'),
+        'publication_date' => $request->input('publication_date'),
+        'content' => $request->input('content'),
+    ]);
+
+    return redirect('/entries');
+});
+
+/* Route::get('/', 'EntryController@index');
+Route::get('/entries', 'EntryController@index')->name('entries.index');
+Route::get('/entries/create', 'EntryController@create')->name('entries.create');
+Route::post('/entries', 'EntryController@store')->name('entries.store');
+
+
+Route::get('/entries', function () {
+    $response = Http::get('http://tu-servicio-rest/api/entries');
+    $entries = $response->json();
+    return view('entries.index', compact('entries'));
+});*/ 
